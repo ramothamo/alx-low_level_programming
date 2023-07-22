6de0728499357ba872a0b35814ffd9df08fc7545
ramothamo <mjramothamo@gmail.com>
2023-07-22 10:20:21 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,23 +6,26 @@
  */
 int main(void)
 {
-        int i;
-        for (i = 1; i <= 100; i++)
-        {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                        printf("FizzBuzz ");
-                } else if (i % 3 == 0)
- 	        {
-        	        printf("Fizz ");
-        	} else if (i % 5 == 0)
-       		 {
-                	printf("Buzz ");
-       		 } else
-       		 {
-                	printf("%d ", i);
-              
-	 	 }
-     }
-    return (0);
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		} 
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		} 
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
 }
