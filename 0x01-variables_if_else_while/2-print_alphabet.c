ceb440771ab54ad64af6ec70c9d284d191d05aab
ramothamo <mjramothamo@gmail.com>
2023-07-31 01:48:49 -0700

    Print Lowercase characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ascii_char = 'a';
+
+	while (ascii_char <= 'z')
+	{
+		putchar(ascii_char);
+		ascii_char++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
