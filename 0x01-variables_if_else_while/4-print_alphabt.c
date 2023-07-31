c71086e94d93fd376ca5a05ecfd6e48e471e6621
ramothamo <mjramothamo@gmail.com>
2023-07-31 03:37:58 -0700

    Print except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 
 	while (ascii_char <= 'z')
 	{
-		if (ascii_char != 'e' || ascii_char != 'q')
+		if (ascii_char != 'e' && ascii_char != 'q')
 		{
 			putchar(ascii_char);
-			ascii_char++;
 		}
+		ascii_char++;
 	}
 	putchar('\n');
 	return (0);
